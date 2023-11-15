8a8d72fe1c64378183ce78d8145cc87cddefbb2c
Aarthi <taarthi1989@gmail.com>
2023-11-15 10:48:05 +0530

    Whatsapplink_code

--- app/src/main/java/com/example/academy1/MainActivity.java
+++ app/src/main/java/com/example/academy1/MainActivity.java
@@ -1,16 +1,21 @@
 package com.example.academy1;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 import android.view.KeyEvent;
 import android.view.View;
+import android.webkit.CookieManager;
+import android.webkit.CookieSyncManager;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.ProgressBar;
-import android.app.AlertDialog;
-import android.content.DialogInterface;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -31,23 +36,14 @@ public class MainActivity extends AppCompatActivity {
         WebSettings webSettings = webView.getSettings();
         webSettings.setJavaScriptEnabled(true);
 
+        // Clear cache and cookies when the app starts
+        clearCacheAndCookies();
+
         // Load Google in the WebView
         webView.loadUrl("https://academy.code5fixer.com/");
 
         // Open links in the WebView, not in the default browser
-        webView.setWebViewClient(new WebViewClient() {
-            @Override
-            public void onPageStarted(WebView view, String url, android.graphics.Bitmap favicon) {
-                // Show the progress bar when a new page starts loading
-                progressBar.setVisibility(View.VISIBLE);
-            }
-
-            @Override
-            public void onPageFinished(WebView view, String url) {
-                // Hide the progress bar when the page finishes loading
-                progressBar.setVisibility(View.GONE);
-            }
-        });
+        webView.setWebViewClient(new CustomWebViewClient());
     }
 
     // Handle the back button press
@@ -84,5 +80,75 @@ public class MainActivity extends AppCompatActivity {
         }
         return super.onKeyDown(keyCode, event);
     }
-}
 
+    // Custom WebViewClient to handle WhatsApp links
+    private class CustomWebViewClient extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            // Check if the URL starts with the WhatsApp scheme
+            if (url.startsWith("whatsapp://")) {
+                openWhatsApp(url);
+                return true;  // Indicates that the WebView should not load the URL
+            }
+
+            // Load other URLs in the WebView
+            return false;
+        }
+
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            // Show the progress bar when a new page starts loading
+            progressBar.setVisibility(View.VISIBLE);
+        }
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            // Hide the progress bar when the page finishes loading
+            progressBar.setVisibility(View.GONE);
+        }
+
+        @Override
+        public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+            super.onReceivedError(view, errorCode, description, failingUrl);
+            // Handle other errors (e.g., display an error message)
+            showErrorDialog("Error Loading Page", "There was an error loading the page. Please try again.");
+        }
+    }
+
+    private void openWhatsApp(String url) {
+        try {
+            // Extract the phone number from the WhatsApp URL
+            String phoneNumber = url.substring(url.lastIndexOf("/") + 1);
+
+            // Create an Intent to open WhatsApp with the phone number
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            intent.setData(Uri.parse("https://api.whatsapp.com/send?phone=" + phoneNumber));
+            startActivity(intent);
+        } catch (Exception e) {
+            e.printStackTrace();
+            // Handle any exceptions that may occur while opening WhatsApp
+        }
+    }
+
+    private void showErrorDialog(String title, String message) {
+        new AlertDialog.Builder(this)
+                .setTitle(title)
+                .setMessage(message)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface arg0, int arg1) {
+                        // Handle OK button click if needed
+                    }
+                })
+                .show();
+    }
+
+    private void clearCacheAndCookies() {
+        // Clear cache
+        webView.clearCache(true);
+
+        // Clear cookies
+        CookieManager cookieManager = CookieManager.getInstance();
+        cookieManager.removeAllCookies(null);
+        cookieManager.flush();
+    }
+}
